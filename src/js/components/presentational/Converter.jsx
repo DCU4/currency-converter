f18f246a975aa5d7e61a1988efbce7b3e21e3a83
Dylan Connor <DylanJConnor4@gmail.com>
2019-11-28 18:56:33 -0500

    add dollar to euro conversion

--- src/js/components/presentational/Converter.jsx
+++ src/js/components/presentational/Converter.jsx
@@ -4,36 +4,58 @@ export class Converter extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      convert: 1*this.props.dollar
-      
+      euroToDollar: 1*this.props.dollar,
+      dollarToEuro: 1*(this.props.euro/this.props.dollar),
+      swapConverter: false
     }
   }
 
   convert = (n) => {
     const input = n.target.value;
-    const convert = input*this.props.dollar;
+    const euroToDollar = input*this.props.dollar;
+    const dollarToEuro = input*(this.props.euro/this.props.dollar);
     // console.log(convert)
     this.setState({
-      convert: convert
+      euroToDollar: euroToDollar,
+      dollarToEuro: dollarToEuro
     });
     
   }
 
+  swapConversions = () => {
+    const swapConverter = this.state.swapConverter;
+    this.setState({
+      swapConverter: swapConverter ? false : true
+    })
+  }
+
 render() {
   // console.log(this.state.convert);
-  const convert = Math.round(this.state.convert*100) / 100;
+  const euroToDollar = Math.round(this.state.euroToDollar*100) / 100;
+  const dollarToEuro = Math.round(this.state.dollarToEuro*100) / 100;
+  const swapConverter = this.state.swapConverter;
   
-  if(this.props){
-    console.log(convert)
+  if(!this.props.euro || !this.props.dollar) {
+    return null;
   }
   // console.log(this.props)
-
+  
     return (
+      !swapConverter ? (
       <div className="converter">
-        <span>$</span><input type="text" onChange={this.convert} placeholder="1"/>
-        <p className="converted"><span>€</span>{convert.toFixed(2)}</p>
+        <p className="input"><span>€</span><input type="text" onChange={this.convert} placeholder={this.props.euro}/></p>
+        <p className="converted"><span>$</span>{euroToDollar.toFixed(2)}</p>
         <button onClick={this.convert}>Have at it </button>
+        <p onClick={this.swapConversions}>€>$</p>
       </div>
+      ) : (
+        <div className="converter">
+        <p className="input"><span>$</span><input type="text" onChange={this.convert} placeholder="1"/></p>
+        <p className="converted"><span>€</span>{dollarToEuro.toFixed(2)}</p>
+        <button onClick={this.convert}>Have at it </button>
+        <p onClick={this.swapConversions}>$>€</p>
+      </div>
+      )
 
     );
   }
